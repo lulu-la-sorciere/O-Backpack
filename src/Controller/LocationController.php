e4ca4d7a3c239ce3cc1f14edbb4f75765b1e9b15
vivikachu <viviane.guidicelli@gmail.com>
2021-07-18 13:05:08 +0200

    location continent country controllers and twig in progress

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Continent;
+use App\Repository\ContinentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/location", name="location_")
+ */
+
 class LocationController extends AbstractController
 {
     /**
-     * @Route("/location", name="location")
+     * @Route("/continent", name="continent")
      */
-    public function index(): Response
+    public function index(ContinentRepository $continentRepository): Response
     {
         return $this->render('location/index.html.twig', [
-            'controller_name' => 'LocationController',
+            'location_continent' => $continentRepository->findAll(),
         ]);
     }
 }
---
+++ src/Entity/Continent.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContinentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContinentRepository::class)
+ */
+class Continent
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Post::class, mappedBy="continent")
+     */
+    private $posts;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Country::class, mappedBy="continent", orphanRemoval=true)
+     */
+    private $countries;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+        $this->countries = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Post[]
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): self
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts[] = $post;
+            $post->addContinent($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): self
+    {
+        if ($this->posts->removeElement($post)) {
+            $post->removeContinent($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Country[]
+     */
+    public function getCountries(): Collection
+    {
+        return $this->countries;
+    }
+
+    public function addCountry(Country $country): self
+    {
+        if (!$this->countries->contains($country)) {
+            $this->countries[] = $country;
+            $country->setContinent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCountry(Country $country): self
+    {
+        if ($this->countries->removeElement($country)) {
+            // set the owning side to null (unless already changed)
+            if ($country->getContinent() === $this) {
+                $country->setContinent(null);
+            }
+        }
+
+        return $this;
+    }
+}
