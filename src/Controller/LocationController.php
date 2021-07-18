1cf56ca7eee0c7d4537d76d3c4533772b18485e8
vivikachu <viviane.guidicelli@gmail.com>
2021-07-18 18:56:14 +0200

    hamburger menu in progress

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Continent;
 use App\Repository\ContinentRepository;
+use App\Repository\CountryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,16 +12,27 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/", name="location_")
  */
-
 class LocationController extends AbstractController
 {
     /**
-     * @Route("/continent", name="continent")
+     * @Route("continent", name="continent")
      */
     public function index(ContinentRepository $continentRepository): Response
     {
         return $this->render('location/index.html.twig', [
             'location_continent' => $continentRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * List of countries according to continent
+     * 
+     * @Route("country", name="country")
+     */
+    public function show(CountryRepository $country): Response
+    {
+        return $this->render('location/country.html.twig', [
+            'country' => $country
         ]);
     }
 }
