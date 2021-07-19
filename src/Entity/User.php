7ccd242ddfae898e8632571eb2617307a9eae8e7
Clay <c.clelia@live.fr>
2021-07-19 15:56:26 +0200

    security log

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User
+class User 
 {
     /**
      * @ORM\Id
