b52fa0f6c42055b119fc1159c1359f89f1392d5d
SamiaBouirou <samia.bouirou@gmail.com>
2021-07-19 13:18:31 +0200

    loop on countries list

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -19,21 +19,35 @@ class LocationController extends AbstractController
      */
     public function index(ContinentRepository $continentRepository): Response
     {
-        
+
         return $this->render('location/index.html.twig', [
             'location_continent' => $continentRepository->findAll(),
         ]);
     }
 
+    /*
     /**
      * List of countries according to continent
      * 
      * @Route("country", name="country")
      */
-    public function show(CountryRepository $country): Response
+    /*public function show(CountryRepository $country): Response
     {
         return $this->render('location/country.html.twig', [
             'country' => $country
+        ]);
+    }
+    */
+
+    /**
+     * @Route("continent/{id}", name="detail")
+     *
+     */
+    public function detail(Continent $continent)
+    {
+        return $this->render('location/countries.html.twig', [
+            'continent' => $continent,
+            'countries' => $continent->getCountries(),
         ]);
     }
 }
