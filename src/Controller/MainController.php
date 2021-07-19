314aa8a9acf4715ab5c470b7199e800dbf36b83b
email <celine.toulet@gmail.com>
2021-07-19 17:01:50 +0200

    test

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/", name="main_")
+ * 
+ */
 class MainController extends AbstractController
 {
     /**
