78bf1155ccb7cf19feca88ac10aa0cfcc2fbb98f
Clay <c.clelia@live.fr>
2021-07-20 12:08:24 +0200

    login and registration finish begin user profil

--- migrations/Version20210720084700.php
+++ migrations/Version20210720084700.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210716125824 extends AbstractMigration
+final class Version20210720084700 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20210716125824 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE country ADD visa VARCHAR(255) NOT NULL, CHANGE vaccine vaccine TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE country DROP visa, CHANGE vaccine vaccine VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user DROP is_verified');
     }
 }
