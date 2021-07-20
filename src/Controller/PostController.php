a2f44bd543a06e7e92669bbc39b48a2b0cf18366
SamiaBouirou <samia.bouirou@gmail.com>
2021-07-20 11:53:00 +0200

    gallery and pictures management

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Continent;
+use App\Repository\ContinentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     /**
+     * Method to blog page
      * @Route("/", name="list")
      */
     public function index(): Response
@@ -22,5 +25,28 @@ class PostController extends AbstractController
         ]);
     }
 
-    
+    /**
+     * Method to picture gallery
+     * @Route("/pictures", name="gallery")
+     */
+    public function galleryPic(ContinentRepository $continentRepository)
+    {
+
+        return $this->render('post/gallery.html.twig', [
+            'continents' => $continentRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * Method to continent's pictures
+     * @Route("/pictures/{id}", name="continentPic")
+     * 
+     */
+    public function continentPic(Continent $continent)
+    {
+        return $this->render('post/picbycontinent.html.twig',[
+            'continent' => $continent,
+
+        ]);
+    }
 }
