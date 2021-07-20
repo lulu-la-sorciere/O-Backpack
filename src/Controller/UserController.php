1aa76b79a01d3c50db3be2f928737df6bf5b401b
vivikachu <viviane.guidicelli@gmail.com>
2021-07-20 16:31:27 +0200

    wip update account by user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +20,18 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+
+    /**
+     * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, User $user, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        //The member can change its personnal information in its account
+
+        $this->denyAccessUnlessGranted('USER_EDIT', $user, 'Accès refusé - Zone protégée');
+
+
     }
 }
