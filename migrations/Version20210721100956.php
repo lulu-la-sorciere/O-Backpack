07c2dc5ede19e6d8987142a62c9373056e789a38
vivikachu <viviane.guidicelli@gmail.com>
2021-07-21 18:57:34 +0200

    update password wip

--- migrations/Version20210721100956.php
+++ migrations/Version20210721100956.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210721085554 extends AbstractMigration
+final class Version20210721100956 extends AbstractMigration
 {
     public function getDescription(): string
     {
