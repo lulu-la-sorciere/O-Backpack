7d683501ae53b14bc654a92e69ea958756f428cc
vivikachu <viviane.guidicelli@gmail.com>
2021-07-23 16:04:35 +0200

    bases ok

--- src/Form/MemberDataType.php
+++ src/Form/MemberDataType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -38,7 +39,7 @@ class MemberDataType extends AbstractType
                 ],*/
             ])
             ->add('country')
-            ->add('dateOfBirth')
+            ->add('dateOfBirth', DateType::class)
             ->add('Valider', SubmitType::class);;
     }
 
