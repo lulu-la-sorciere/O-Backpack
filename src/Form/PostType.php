7c9ffe49a8ede29210fae54260e890b2683e96fc
SamiaBouirou <samia.bouirou@gmail.com>
2021-07-25 21:38:32 +0200

    style_post_add_form_WIP

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,12 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Continent;
+use App\Entity\Country;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Choice;
@@ -18,14 +21,30 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('picture')
-            ->add('continent')
-            ->add('country')
+            ->add('title', TypeTextType::class, array(
+                'label' => 'titre',
+                'attr' => array(
+                    'placeholder' => 'Titre'
+                )
+            ))
+            ->add('content', TextareaType::class, array(
+                'label' => 'Contenu',
+                'attr' => array(
+                    'placeholder' => 'Votre article'
+                )
+            ))
+            ->add('continent', ChoiceType::class, [
+                /*'choice_value' => 'name',*/
+            ])
+            ->add('country', ChoiceType::class,[
+                'choices' => [
+                    new Country('Country1'),
+                    new Country('Country2')],
+                'choice_value' => 'name',
+            ])
             //->add('user')
             ->add('picture', FileType::class, [
-                'label' => 'Téléverser une image',
+                'label' => 'Ajouter une image',
                 'required' =>false,
                 ])
             ->add('Valider', SubmitType::class);
