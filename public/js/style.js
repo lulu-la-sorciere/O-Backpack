90e8fe681a8b18c3ebe19dee3594625870baf31b
vivikachu <viviane.guidicelli@gmail.com>
2021-07-27 20:03:15 +0200

    wip forms templates

--- public/js/style.js
+++ public/js/style.js
@@ -75,3 +75,15 @@ var repeat = function(activeClass){
   repeater();
 }
 repeat();
+
+// forms
+
+$('input').on('focusin', function() {
+  $(this).parent().find('label').addClass('active');
+});
+
+$('input').on('focusout', function() {
+  if (!this.value) {
+    $(this).parent().find('label').removeClass('active');
+  }
+});
