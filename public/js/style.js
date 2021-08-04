87dda14c7106146a4eee3b0db14225c3486cb7b6
email <celine.toulet@gmail.com>
2021-08-04 11:03:30 +0200

    nav

--- public/js/style.js
+++ public/js/style.js
@@ -10,14 +10,14 @@ function mobileMenu() {
 
 // nav element color changes when current page is active
 
-/* const currentLocation = location.href;
+const currentLocation = location.href;
     const menuItem = document.querySelectorAll('a');
     const menuLength = menuItem.length
         for (let i=0; i<menuLength; i++){
             if(menuItem[i].href === currentLocation){
                 menuItem[i].className = "active"
                 }
-            } */
+            } 
 
 // SLIDER
 
