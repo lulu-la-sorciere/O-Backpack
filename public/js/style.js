52c43f84e0cc191100a1439422a87d84fbe6f93d
email <celine.toulet@gmail.com>
2021-08-04 14:48:10 +0200

    update picture

--- public/js/style.js
+++ public/js/style.js
@@ -10,7 +10,7 @@ function mobileMenu() {
 
 // nav element color changes when current page is active
 
-const currentLocation = location.href;
+ const currentLocation = location.href;
     const menuItem = document.querySelectorAll('a');
     const menuLength = menuItem.length
         for (let i=0; i<menuLength; i++){
